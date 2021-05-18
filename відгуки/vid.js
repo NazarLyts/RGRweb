f58cc1087be4c52e0111c70f33da99bd81568d8a
NazarLyts <67822094+NazarLyts@users.noreply.github.com>
2021-05-18 17:35:04 +0300

    Add files via upload

---
+++ відгуки/vid.js
@@ -0,0 +1,10 @@
+function print_in(){
+    var InputIn1 = document.getElementById('input-in1').value;
+    var InputIn2 = document.getElementById('input-in2').value;
+    var p = document.getElementById('demo');
+    var p2 = document.getElementById('demo2');
+    p.innerHTML = InputIn1;
+    p2.innerHTML = InputIn2; 
+}
+
+
